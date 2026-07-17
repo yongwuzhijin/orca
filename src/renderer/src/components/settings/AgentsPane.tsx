f3e8d76b1f5c1f9dae01d4f7d337fabc5a006a60
佑平 <jace.cj@alibaba-inc.com>
2026-07-17 23:08:01 +0800

    feat(todo): add global orchestrator settings UI

--- src/renderer/src/components/settings/AgentsPane.tsx
+++ src/renderer/src/components/settings/AgentsPane.tsx
@@ -11,6 +11,7 @@ import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 import { cn } from '@/lib/utils'
 import { AgentAwakeSetting } from './AgentAwakeSetting'
+import { TodoOrchestratorSetting } from './TodoOrchestratorSetting'
 import { AgentCacheTimerSection } from './AgentCacheTimerSection'
 import { AgentRuntimeSetting } from './AgentRuntimeSetting'
 import {
@@ -834,6 +835,8 @@ export function AgentsPane({
       <AgentGeneratedTabTitlesSetting settings={settings} updateSettings={updateSettings} />
 
       <AgentAwakeSetting settings={settings} updateSettings={updateSettings} />
+
+      <TodoOrchestratorSetting settings={settings} updateSettings={updateSettings} />
 
       <AgentCacheTimerSection settings={settings} updateSettings={updateSettings} />
 
